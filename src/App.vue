5619f410f2ec164018e195af4a35ed33b7900ac4
Sasha <sasha.shvets.2017@mail.ru>
2023-11-29 16:40:14 +0200

    feat: setting tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
+  <div class="container mx-auto flex flex-col items-center p-4">
     <div class="container">
       <section>
         <div class="flex">
@@ -177,5 +177,3 @@ export default {
   },
 };
 </script>
-
-<style src="./app.css"></style>
